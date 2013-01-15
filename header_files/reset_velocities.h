7bd78a9d19be5b8a6b8f7afe1463720a32ce3028
bryanfoley <shagoff@gmail.com>
2013-01-15 08:04:39 +0100

    Basic tracing added

--- header_files/reset_velocities.h
+++ header_files/reset_velocities.h
@@ -2,6 +2,11 @@ void reset_velocities(double free_particle[n_max][m_max][z_max],
                         double image_free_particle[n_max][m_max][z_max])
 {
        int i,l;
+       char func_name[30] = "reset_velocities";
+       char action_begin[10] = ">>>";
+       char action_end[10] = "<<<";
+       time_stamp();
+       trace(func_name,action_begin);
        
        for(i = 0; i < n_max; i++)
         {
@@ -13,4 +18,6 @@ void reset_velocities(double free_particle[n_max][m_max][z_max],
            image_free_particle[i][1][l] = 0.0;
           }
         }
+      time_stamp();
+      trace(func_name,action_end);
 }
