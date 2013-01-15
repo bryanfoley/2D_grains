3264e315621ff6eac99dd778f3d8eabe657442c2
bryanfoley <shagoff@gmail.com>
2013-01-15 07:51:24 +0100

    Basic tracing added

--- header_files/old_acc.h
+++ header_files/old_acc.h
@@ -2,6 +2,11 @@ void old_acc(double free_particle[n_max][m_max][z_max],
              double image_free_particle[n_max][m_max][z_max])
 {
      int i;
+     char func_name[30] = "old_acc";
+     char action_begin[10] = ">>>";
+     char action_end[10] = "<<<";
+     time_stamp();
+     trace(func_name,action_begin);
      
      /*Calculate the acceleration of the free_particles and image_free_particles*/
      for(i=0; i<(n_max); i++)
@@ -16,4 +21,6 @@ void old_acc(double free_particle[n_max][m_max][z_max],
        /*Acceleration of image_free_particle 'i', y component*/
        image_free_particle[i][5][0] = ((image_free_particle[i][7][0])/(image_free_particle[i][8][0]));
       }
+     time_stamp();
+     trace(func_name,action_end);
 }
