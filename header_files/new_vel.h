b93390dcc00118809bce461e494af45056b16806
bryanfoley <shagoff@gmail.com>
2013-01-15 07:50:17 +0100

    Basic tracing added

--- header_files/new_vel.h
+++ header_files/new_vel.h
@@ -2,6 +2,11 @@ void new_vel(double free_particle[n_max][m_max][z_max],
              double image_free_particle[n_max][m_max][z_max])
 {
      int i;
+     char func_name[30] = "new_vel";
+     char action_begin[10] = ">>>";
+     char action_end[10] = "<<<";
+     time_stamp();
+     trace(func_name,action_begin);
      
      for(i = 0; i < n_max; i++)
       {
@@ -15,4 +20,6 @@ void new_vel(double free_particle[n_max][m_max][z_max],
        image_free_particle[i][1][1] = image_free_particle[i][1][0]
                            +(((image_free_particle[i][5][0] + image_free_particle[i][5][1])/2.0000000000000000)*(dt));
       }
+    time_stamp();
+    trace(func_name,action_end);
 }
