85d4fa6a348aa97a99598be1eb9bf19fac48860c
bryanfoley <shagoff@gmail.com>
2013-01-15 06:10:27 +0100

    Tidy up the formatting

--- header_files/trace.h
+++ header_files/trace.h
@@ -8,10 +8,18 @@ void *time_stamp( ){
 	struct tm *tm;
 	tm=localtime(&ltime);
 
-	fprintf(fptr_trace,"%04d%02d%02d%02d%02d%02d, ", tm->tm_year+1900, tm->tm_mon,tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+	fprintf(fptr_trace,"%04d%02d%02d%02d%02d%02d, ", 
+                 tm->tm_year+1900,
+                 tm->tm_mon,
+                 tm->tm_mday,
+                 tm->tm_hour,
+                 tm->tm_min,
+                 tm->tm_sec);
 }
 
 void trace( char func_name[], char action[] ){
-        fprintf(fptr_trace,"%s %s \n", &func_name[0], &action[0]);
+        fprintf(fptr_trace,"%s %s \n",
+                &func_name[0],
+                &action[0]);
 }
 /*EOF*/
