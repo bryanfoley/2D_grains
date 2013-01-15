078eae308ea6898d80c0611d358f2e3212c6614d
bryanfoley <shagoff@gmail.com>
2013-01-15 06:34:05 +0100

    Basic tracing added

--- header_files/average_velocity_y.h
+++ header_files/average_velocity_y.h
@@ -2,6 +2,11 @@ double average_velocity_y(double free_particle[n_max][m_max][z_max])
 {
        double sumy = 0.0;
        double ans = 0.0;
+       char func_name[30] = "average_velocity_y";
+       char action_begin[10] = ">>>";
+       char action_end[10] = "<<<";
+       time_stamp();
+       trace(func_name,action_begin);
        
        for(i = 0; i < n_max; i++)
         {
@@ -9,6 +14,8 @@ double average_velocity_y(double free_particle[n_max][m_max][z_max])
         }
        
        ans = (sumy)/n_max;
+       time_stamp();
+       trace(func_name,action_end);
        
        return(ans);
 }
