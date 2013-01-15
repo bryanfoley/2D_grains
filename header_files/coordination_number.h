c5bbebf072ab32145b4318243496a8031e927fae
bryanfoley <shagoff@gmail.com>
2013-01-15 06:49:32 +0100

    Basic tracing added

--- header_files/coordination_number.h
+++ header_files/coordination_number.h
@@ -3,6 +3,11 @@ double coordination_number(double free_particle[n_max][m_max][z_max])
        double sum = 0.0;
        double ans;
        int i,j,k;
+       char func_name[30] = "coordination_number";
+       char action_begin[10] = ">>>";
+       char action_end[10] = "<<<";
+       time_stamp();
+       trace(func_name,action_begin);
        
        for(i = 0; i < n_max; i++)
        {
@@ -12,4 +17,6 @@ double coordination_number(double free_particle[n_max][m_max][z_max])
        ans = ((sum/n_max));
        
        return(ans);
+       time_stamp();
+       trace(func_name,action_end);
 }
